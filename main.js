6687126288d3eded87c544843a7055e624f439c8
Biadetis <j.j.p@mail.de>
2024-12-03 10:28:48 +0100

    keine Ahnung man

--- main.js
+++
@@ -1,7 +0,0 @@
-document
-  .querySelector("Meine-Projekte")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    const target = document.querySelector("#Meine-Projekte");
-    target.scrollIntoView({ behavior: "smooth" });
-  });
